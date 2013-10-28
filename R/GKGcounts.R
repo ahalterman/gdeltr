435a27d0f85408413c2ab1eaa45dec10a4f8f4a9
ahalterman <ahalterman0@gmail.com>
2013-10-28 12:30:28 -0400

    updated documentation

--- R/GKGcounts.R
+++ R/GKGcounts.R
@@ -3,7 +3,7 @@
 #' The Global Knowledge Graph contains two elements, the "Counts" file, containing information on the numbers of people killed, affected, etc. every day by location.
 #' The second file, the "Graph file", contains the associated themes, organizations, people, and locations.  Filtering, especially by themes, is very useful, but much of the useable information in the GKG is in the Counts file.  This function will return the Counts file, nicely formatted (it's \code{;} and \code{#} separated, which is a hassle), in no particular order.  It loses the date and theme information, though, which is the next room for improvement.
 #'
-#' @param gkg A subset dataframe of the GKG.
+#' @param gkg A subset dataframe of the GKG. \code{gkg}
 #'
 #' @return counts A data frame containing information from the \code{Counts} column. 
 #'
