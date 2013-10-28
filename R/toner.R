0b563c864547d3df0d2b2e1e38b62e183c0f851d
ahalterman <ahalterman0@gmail.com>
2013-10-28 13:45:19 -0400

    added rudimentary error catching

--- R/toner.R
+++ R/toner.R
@@ -1,10 +1,10 @@
 #' Given a GKG subset, return the tones associated with each person/place/organization
 #' 
-#' summarize will return the mean tone for each entity.
-#' eventually, include type Return tone for persons, countries, locations, or organizations? \code{type}
+#' summarize will return the mean tone for each entity.  This feature isn't done yet.
 #'
 #' @param df A subset of the GKG, probably along one theme\code{df}
-#' @param summarize Should the mean for each unique entitty be returned?  Caution: lots of alt. spellings \code{summarize}
+#' @param type Return tones of organization, locations, or persons?
+#' @param summarize Should the mean for each unique entity be returned?  Caution: lots of alt. spellings \code{summarize}
 #'
 #' @return tones A df with names/locations and tones (and counts if summarized).
 #'
@@ -14,17 +14,22 @@
 #' 
 #' @examples
 #' > ieds <- gkg[grep("LANDMINE", gkg$THEMES),]
-#' > person.tone.ieds <- toner(ieds)
+#' > person.tone.ieds <- toner(ieds, type="persons")
 #' > dim(person.tone.ieds)
 #' [1] 4545    2
 
 
-toner <- function(df){
+toner <- function(df, type){
+  if (!"TONE" %in% names(df)) stop("No column named 'TONE' in input data frame")
+  if (rnow(df)==0) stop("Input data frame has 0 rows")
+  
+  if (type=="Person" | "person" | "persons" | "PERSONS") {
   persontone <- data.frame(stringsAsFactors=FALSE)
+  if (!"PERSONS" %in% names(df)) stop("No column named 'PERSONS' in input data frame")
   for (i in 1:nrow(df)) {
     line <- df[i,]
-    persons <- line[,5]
-    tone <- line[,7]
+    persons <- line$PERSONS
+    tone <- line$TONE
     tone <- unlist(strsplit(tone, ","))
     tone <- as.numeric(tone[1])
     persons <- as.character(unlist(strsplit(persons, ";")))
@@ -36,4 +41,13 @@ toner <- function(df){
   persontone$V2 <- as.numeric(as.character(persontone$V2))
   persontone <- as.data.frame(summarise(group_by(tbl_df(persontone), persons), count=n(), meantone=mean(V2)))
   return(persontone)
+  }
+  #  if (type=="locations" | "LOCATIONS") {
+  #   if (!"LOCATIONS" %in% names(df)) stop("No column named 'LOCATIONS' in input data frame")
+  # }
+
+  # if (summarize=TRUE){
+  # take the df, group_by column 1, column 2=mean(column2)
+  # return
+  # }
 }
